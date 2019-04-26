e64b4a09cc7db7283e6c775909afb22554634479
Zenas Muli <zenas2294@gmail.com>
2019-04-26 12:24:04 +0300

    Moved quote array to component and linked it to app.component.html after moving the html to the component as well

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1, 'Si ni kenyewe kimejificha kwenye kichwa'),
+    new Quote(2, 'Zimenishika yo'),
+    new Quote(3, 'Everything you can Imagine is real'),
+    new Quote(4, 'Tuendelee ama tusiendelee'),
+    new Quote(5, 'Kawia ufike'),
+    new Quote(6, 'Zubaa zubaa utapata mwana si wako')
+
+  ]
 
   constructor() { }
 
