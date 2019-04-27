92f1a85df5c6e39168edd8c165e628567b1f78ef
Zenas Muli <zenas2294@gmail.com>
2019-04-27 22:58:32 +0300

    Created highlight directive for the quotes that changes color when clicked. Should remember to change it to the quote with the highest quotes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+import { Quote } from './quote';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input() quote:Quote;
+
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.backgroundColor = 'None';
+  }
+
+  @HostListener('click') onClicks() {
+    this.highlight('red');
+  }
+
+  @HostListener('dblclick') onDoubleClicks() {
+    this.highlight('None');
+  }
+
+  private highlight(action: string){
+    this.elem.nativeElement.style.backgroundColor = action;
+  }
 
 }
